3a20fde306759a7dbec2a1ae54405058ad11b378
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-04-19 21:51:10 +0700

    Set a maximum length of 2 lines and the displayed text is automatically truncated if it is too long

--- app/src/main/java/com/rujirakongsomran/kt_jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_jetpackcomposetutorial/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -57,8 +58,12 @@ class MainActivity : AppCompatActivity() {
 
                 Spacer(Modifier.height(16.dp))
                 Text(
-                    "A day in Shark Fin Cove",
-                    style = typography.h6
+                    "A day wandering through the sandhills " +
+                            "in Shark Fin Cove, and a few of the " +
+                            "sights I saw",
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
                 )
                 Text(
                     "Davenport, California",
