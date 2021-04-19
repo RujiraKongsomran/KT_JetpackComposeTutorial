687b6efe1c3e59e15c3fcc67d7709d964c476de8
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-04-19 21:18:00 +0700

    Add picture

--- app/src/main/java/com/rujirakongsomran/kt_jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_jetpackcomposetutorial/MainActivity.kt
@@ -3,11 +3,13 @@ package com.rujirakongsomran.kt_jetpackcomposetutorial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -38,6 +40,16 @@ class MainActivity : AppCompatActivity() {
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
+            Image(
+                painter = painterResource(R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(Modifier.height(16.dp))
             Text("A day in Shark Fin Cove")
             Text("Davenport, California")
             Text("December 2018")
