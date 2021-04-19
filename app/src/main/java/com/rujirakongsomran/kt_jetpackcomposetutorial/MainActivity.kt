6dbb5465abf16ee725e007bfad774c5ed59fd152
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-04-19 14:47:19 +0700

    Define a composable function

--- app/src/main/java/com/rujirakongsomran/kt_jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_jetpackcomposetutorial/MainActivity.kt
@@ -2,10 +2,22 @@ package com.rujirakongsomran.kt_jetpackcomposetutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        setContent {
+            Greeting("Android")
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name")
     }
 }
